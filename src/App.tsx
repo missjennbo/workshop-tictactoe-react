b687830d43af5c94605e18e4510df23b42bef5eb
Jennifer Lucifero <jennifer.lucifero@codecentric.de>
2022-06-03 14:31:26 +0200

    Introduce redux for state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import GameView from './components/Game/GameView';
+import {Provider} from 'react-redux';
+import {store} from './store';
 
 const App = (): JSX.Element => (
-    <div>
-        <GameView></GameView>
-    </div>
+    <Provider store={store}>
+        <GameView />
+    </Provider>
 );
 
 export default App;
--- src/components/Game/GameView.tsx
+++ src/components/Game/GameView.tsx
@@ -1,28 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 import BoardView from '../Board/BoardView';
-import {getInitialBoard} from './initialBoard';
-import {displayWinner, getNextBoard, getNextPlayer, isGameFinished} from '../../logic/game';
+import {displayWinner} from '../../logic/game';
 import styles from './GameView.module.scss';
-import {Cell, Player} from '../types';
+import {Cell} from '../types';
+import {useDispatch, useSelector} from 'react-redux';
+import {CELL_CLICKED, RESET_GAME} from '../../actions/gameActions';
+import {GameAction} from '../../reducer/gameReducer';
+import {RootState} from '../../store';
 
 const GameView = (): JSX.Element => {
-    const [board, setBoard] = useState(getInitialBoard());
-    const [gameFinished, setGameFinished] = useState(false);
-    const [currentPlayer, setCurrentPlayer] = useState(Player.heart);
+    const board = useSelector((state: RootState) => state.gameReducer.board);
+    const gameFinished = useSelector((state: RootState) => state.gameReducer.gameFinished);
+    const currentPlayer = useSelector((state: RootState) => state.gameReducer.currentPlayer);
+    const dispatch = useDispatch();
 
-    const onCellClick = (cell: Cell): void => {
-        if (gameFinished) {
-            return;
-        }
-        setBoard(getNextBoard(board, cell, gameFinished, currentPlayer));
-        setGameFinished(isGameFinished(board, currentPlayer));
-        setCurrentPlayer(getNextPlayer(currentPlayer));
-    };
-
-    const resetGame = (): void => {
-        setBoard(getInitialBoard());
-        setGameFinished(false);
-    };
+    const onCellClick = (clickedCell: Cell): GameAction => dispatch({type: CELL_CLICKED, clickedCell: clickedCell});
+    const resetGame = (): GameAction => dispatch({type: RESET_GAME});
 
     return (
         <div className={styles['container']}>
